fef52724f55021ebb19b616bd7ee8bfff45e8ac4
unknown <shivamkoolwal14@gmail.com>
2021-06-08 10:16:27 +0530

    chnages in cart.vue

--- client/src/views/Cart.vue
+++ client/src/views/Cart.vue
@@ -1,5 +1,14 @@
 <template>
 <v-container>
+  <h1>Your cart</h1>
+  <v-btn 
+    elevation="2"
+    large
+    color = "white"
+    class = "black--text"
+    depressed
+    @click = "navigateTo({name : 'home'})"
+  ></v-btn>
   <v-simple-table class="mt-10">
     <template>
       <thead>
@@ -15,24 +24,33 @@
           :key="item.name"
         >
           <td class="mb-3" style="height:100px;">
-            <div class="mt-3 pb-3">
+            <div class="mt-3 mb-3">
                 <v-row class="mt-1">
                   <v-card
                     width="120"
                     flat
                   >
-                  <v-img
-                      :src = "item.src"
-                      aspect-ratio = 1
-                  ></v-img>
+                      <v-img
+                          :src = "item.src"
+                          aspect-ratio = 1
+                      ></v-img>
                   </v-card>
                   <v-card
-                    width="100"
+                    width="150"
                     flat
                   >
-                    <v-card-text>
-                      <p class="grey--text text--darken-3">Colour Lush</p>
-                    </v-card-text>
+                      <v-card-text>
+                        <p class="grey--text text--darken-3">Item Name</p>
+                        <v-btn
+                            v-model = "selected"
+                            small
+                            color ="white"
+                            class ="black--text"
+                            depressed
+                            elevation="2"
+                            @click = deleteitem()
+                        >Delete</v-btn>
+                      </v-card-text>
                   </v-card>
                 </v-row>
               </div>
@@ -50,6 +68,7 @@
 export default {
   data () {
       return {
+        selected: [],
         headers: [
           {
             text: 'Product',
@@ -60,7 +79,7 @@ export default {
           },
           { text: 'Price', value: 'price' , sortable:false },
           { text: 'Quantity', value: 'quantity' , sortable:false },
-          { text: 'Total', value: 'total' , sortable:false},
+          { text: 'Total', value: 'total' , sortable:false },
         ],
         products: [
           {
@@ -90,33 +109,44 @@ export default {
           },
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            name : 'Item5',
+            name : 'Item6',
             price : 500
           },
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            name : 'Item5',
+            name : 'Item7',
             price : 500
           },
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            name : 'Item5',
+            name : 'Item8',
             price : 500
           },
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            name : 'Item5',
+            name : 'Item9',
             price : 500
           },
           {
             src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            name : 'Item5',
+            name : 'Item10',
             price : 500
           },
         ],
       }
     },
-  }
+    methods : {
+      deleteitem() {
+          this.selected.forEach((gg)=>{
+              console.log(gg);
+          });
+           // this.selected.forEach(function(project) {
+           // this.projects.splice(project, 1);
+           //  });
+      this.selected = [];
+    }
+  },
+}
 </script>
 
 <style>
